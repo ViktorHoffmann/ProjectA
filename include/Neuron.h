f042dfca94e3113e2d698bb3be38abad3b4d3ca7
Viktor Hoffmann <viktor.m.hoffmann@gmail.com>
2020-10-07 12:45:19 +0200

    Test Repo

---
+++ include/Neuron.h
@@ -0,0 +1,19 @@
+#pragma once
+// Class for Neuron object
+
+#include <cmath>
+
+class Neuron{
+public:
+	double Input_value = NULL;
+	double Activated_valu = NULL;
+	double deriv_Activated_value = NULL;
+private:
+	double sigmoid(double Input_value){
+		return Activated_value = ((1)/(1+exp(-(Input_value))));
+	}
+
+	double deriv_sigmoid(double Input_value){
+		return deriv_Activated_value = (sigmoid(Input_value)*(1-sigmoid(Input_value)));
+	}
+};
