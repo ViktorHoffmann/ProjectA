a2cfb260cf968bb04915f0810d5ff6913099990b
Viktor Hoffmann <viktor.m.hoffmann@gmail.com>
2020-10-12 11:20:56 +0200

    obsolete seeding removed

--- src/Pseudorandom.cpp
+++ src/Pseudorandom.cpp
@@ -2,14 +2,8 @@
 #include <iostream>
 //
 #include "Pseudorandom.h"
-bool seeded = false;
 
 double rand_num(double rand_num_min, double rand_num_max){
-    if (seeded == false)
-    {
-        srand (time(NULL));
-        seeded = true;
-    }
     std::random_device re;
     std::mt19937 gen(re());
     std::uniform_real_distribution<double> unif(rand_num_min, rand_num_max);
