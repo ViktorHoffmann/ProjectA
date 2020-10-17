f8d385b744068831b0adff7cea8c679afc2c848e
Viktor Hoffmann <viktor.m.hoffmann@gmail.com>
2020-10-17 19:15:51 +0200

    User input added

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 int main() {
 	int nLayers = 0;
-	std::vector<int> sizeLayers = {};
+	std::vector<int> neuronLayers = {};
 
 	std::cout << "Number of layers: ";
 	std::cin >> nLayers;
@@ -18,10 +18,10 @@ int main() {
 		for (int i = 0; i < nLayers; i++)
 		{
 			int size = 0;
-			std::cout << "Layer " << i << " size: ";
+			std::cout << "Layer " << i+1 << " size: ";
 			std::cin >> size;
 			std::cout << "\n";
-			sizeLayers.push_back(size);
+			neuronLayers.push_back(size);
 		}
 	}
 	else
@@ -30,24 +30,34 @@ int main() {
 		return 1;
 	}
 	
-	//set the size of the Neuron and Weight vectors according to the input vector
-	std::vector<std::vector<Neuron>>
-	l(0, std::vector<Neuron>(0));
-	
-	std::vector<std::vector<std::vector<Weight>>>
-	w(0, std::vector<std::vector<Weight>>(0, std::vector<Weight>(0)));
+	std::vector<std::vector<Neuron>> l;
+	std::vector<std::vector<std::vector<Weight>>> w;
 
-	for (int j = 0; j <= 3; j++)
+	//set the size of the Neuron and Weight vectors according to the input vector
+	std::cout << "> building network...\n";
+
+	for (int k = 0; k < neuronLayers.size(); k++)//layer iterator
 	{
-		for (int i = 0; i <= 3; i++)
+		for (int j = 0; j < neuronLayers[k]/*start-layer size*/; j++)
 		{
-			l[0][j].init_bias(0,1,0.5,1);
-			l[1][j].init_bias(0,1,0.5,1);
-			l[0][j].init_input(0,1);
-			w[0][j][i].init_weight(0,1,0.5,1);
+			//neuron initialization
+			l.resize(neuronLayers.size(),std::vector<Neuron>(neuronLayers[k]));
+			l[k][j].init_bias(0,1,0.5,1);
+			if (neuronLayers[k] != neuronLayers.size())//if not last layer: build weights
+			{
+				for (int i = 0; i < neuronLayers[k+1]/*end-layer size*/; i++)
+				{
+					//weight initialization
+					w.resize(neuronLayers.size(), std::vector<std::vector<Weight>>(neuronLayers[k], std::vector<Weight>(neuronLayers[k+1])));
+					w[k][j][i].init_weight(0,1,0.5,1);
+				}
+			}
 		}
 	}
 
+	std::cout << "> network build succesfully\n";
+
+	/*
 	for (int j = 0; j < (sizeof(l[1])/sizeof(l[1][0])); j++)
 	{
 		for (int i = 0; i < (sizeof(l[0])/sizeof(l[0][0])); i++)
@@ -57,4 +67,5 @@ int main() {
 			l[1][j].deriv_Activated_value_ = l[1][j].deriv_sigmoid(l[1][j].input_value_);
 		}
 	}
+	*/
 }
